d738949356dd68b28e5f2978876b3440ad3778ed
poxbabs <nickolai.ernst@hotmail.com>
2019-02-14 16:15:24 +0100

    Added an attempt at Camera

--- SMAP/L3/NL/Lab3_2/app/src/main/java/com/nickolailisberglundby/lab3_2/ViewActivity.java
+++ SMAP/L3/NL/Lab3_2/app/src/main/java/com/nickolailisberglundby/lab3_2/ViewActivity.java
@@ -1,18 +1,24 @@
 package com.nickolailisberglundby.lab3_2;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ViewActivity extends AppCompatActivity {
     Button btnToEditView;
+    Button btnToCameraView;
     TextView txtResponseEditView;
+    ImageView imageView;
+
     public final static int REQUEST_TO_EDIT_VIEW = 100;
+    public final static int REQUEST_TO_CAMERA_VIEW = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,16 @@ public class ViewActivity extends AppCompatActivity {
             }
         });
 
+        btnToCameraView = findViewById(R.id.btn_toCameraView);
+        btnToCameraView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnToCameraViewClick();
+            }
+        });
+
+        imageView = findViewById(R.id.img_viewActivity);
+
         txtResponseEditView = findViewById(R.id.textView_viewActivity);
     }
 
@@ -34,6 +50,12 @@ public class ViewActivity extends AppCompatActivity {
     {
         Intent toEditView = new Intent(this, EditActivity.class);
         startActivityForResult(toEditView, REQUEST_TO_EDIT_VIEW);
+    }
+
+    private void btnToCameraViewClick()
+    {
+        Intent toCameraView = new Intent(this, CameraActivity.class);
+        startActivityForResult(toCameraView, REQUEST_TO_CAMERA_VIEW);
     }
 
     @Override
@@ -46,6 +68,18 @@ public class ViewActivity extends AppCompatActivity {
                     case RESULT_OK:
                         txtResponseEditView.setText(data.getExtras().getString("input"));
                         break;
+                    case RESULT_CANCELED:
+                        break;
+
+                    default:
+                        break;
+                }
+            case REQUEST_TO_CAMERA_VIEW:
+                switch(resultCode){
+                    case RESULT_OK:
+                        Bitmap photo = (Bitmap) data.getParcelableExtra("picture");
+                        imageView.setImageBitmap(photo);
+
                     case RESULT_CANCELED:
                         break;
 
