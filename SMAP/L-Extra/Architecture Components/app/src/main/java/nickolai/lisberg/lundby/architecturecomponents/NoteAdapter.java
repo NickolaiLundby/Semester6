964ca559788e1efab8c1b6f9da59425755853eb8
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2019-04-26 12:54:34 +0200

    Architecture tutorial done

---
+++ SMAP/L-Extra/Architecture Components/app/src/main/java/nickolai/lisberg/lundby/architecturecomponents/NoteAdapter.java
@@ -0,0 +1,83 @@
+package nickolai.lisberg.lundby.architecturecomponents;
+
+import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends ListAdapter<Note, NoteAdapter.NoteHolder> {
+    private OnItemClickListener listener;
+
+    public NoteAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    private static final DiffUtil.ItemCallback<Note> DIFF_CALLBACK = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle()) &&
+                    oldItem.getDescription().equals(newItem.getDescription()) &&
+                    oldItem.getPriority() == newItem.getPriority();
+        }
+    };
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.note_item, viewGroup, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int i) {
+        Note currentNote = getItem(i);
+        noteHolder.textViewtitle.setText(currentNote.getTitle());
+        noteHolder.textViewDescription.setText(currentNote.getDescription());
+        noteHolder.textViewPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    public Note getNoteAt(int position) {
+        return getItem(position);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewtitle, textViewDescription, textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewtitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null && getAdapterPosition() != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(getItem(getAdapterPosition()));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Note note);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+}
--- SMAP/L-Extra/Architecture Components/app/src/main/java/nickolai/lisberg/lundby/architecturecomponents/NoteRepository.java
+++ SMAP/L-Extra/Architecture Components/app/src/main/java/nickolai/lisberg/lundby/architecturecomponents/NoteRepository.java
@@ -21,15 +21,15 @@ public class NoteRepository {
     }
 
     public void update(Note note) {
-
+        new UpdateNoteAsyncTask(noteDao).execute(note);
     }
 
     public void delete(Note note) {
-
+        new DeleteNoteAsyncTask(noteDao).execute(note);
     }
 
-    public void deleteAllNotes(Note note) {
-
+    public void deleteAllNotes() {
+        new DeleteAllNotesAsyncTask(noteDao).execute();
     }
 
     public LiveData<List<Note>> getAllNotes() {
@@ -46,6 +46,48 @@ public class NoteRepository {
         @Override
         protected Void doInBackground(Note... notes) {
             noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        private UpdateNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        private DeleteNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private DeleteAllNotesAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
             return null;
         }
     }
